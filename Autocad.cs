bf493301ee463ece2e32a83eda10b43104a15079
asmisloff <a.smisloff@gmail.com>
2016-10-16 21:33:03 +0300

    Autocad as Singleton class

---
+++ Autocad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace spf3
+{
+    class Autocad
+    {
+        static Document activeDoc;
+        static Database activeDB;
+        public static Editor Ed { get; set; }
+
+        static Autocad()
+        {
+            activeDoc = Application.DocumentManager.MdiActiveDocument;
+            activeDB = activeDoc.Database;
+            Ed = activeDoc.Editor;
+        }
+
+        public static SelectionSet SSGet()
+        {
+            PromptSelectionResult prompt = Ed.GetSelection();
+            if (prompt.Status == PromptStatus.OK)
+                return prompt.Value;
+            else
+                return null;
+        }
+
+        public static void SSForeach<T>(SelectionSet sset, Action<T> proc, OpenMode openMode = OpenMode.ForRead)
+            where T : class
+        {
+            if (sset == null)
+                return;
+            using (Transaction tr = activeDB.TransactionManager.StartTransaction()) {
+                foreach (SelectedObject so in sset) {
+                    if (so != null) {
+                        T ent = tr.GetObject(so.ObjectId, openMode) as T;
+                        if (ent != null)
+                            proc(ent);
+                    }
+                }
+                tr.Commit();
+                tr.Dispose();
+            }
+        }
+
+        public static Handle AppendToPaperSpace(Entity ent)
+        {
+            using (Transaction tr = activeDB.TransactionManager.StartTransaction()) {
+                BlockTable bt = tr.GetObject(activeDB.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord paperSpace = tr.GetObject(bt[BlockTableRecord.PaperSpace], OpenMode.ForWrite) as BlockTableRecord;
+                paperSpace.AppendEntity(ent);
+                tr.AddNewlyCreatedDBObject(ent, true);
+                tr.Commit();
+                tr.Dispose();
+            }
+            return ent.Handle;
+        }
+
+    }
+}
